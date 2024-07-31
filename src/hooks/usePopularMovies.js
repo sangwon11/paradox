75e8249a931ef69a2adaca2c7e747476c38f4e19
sangwon11 <dbstkd0408@gmail.com>
2024-07-31 16:01:17 +0900

    역사 장르

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,2 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../utils/api";
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -4,16 +4,16 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Outlet } from 'react-router-dom';
+import './AppLayout.style.css';
 
 
 const AppLayout = () => {
   return (
-    <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
+    <div id='navbar'>
+      <Navbar expand="lg" className="navbar-bg">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Brand href="" className='logo'>Paradox</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
@@ -21,21 +21,8 @@ const AppLayout = () => {
               style={{ maxHeight: '100px' }}
               navbarScroll
             >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
+              <Nav.Link href="/" className='link'>Home</Nav.Link>
+              <Nav.Link href="/movies" className='link'>Movies</Nav.Link>
             </Nav>
             <Form className="d-flex">
               <Form.Control
@@ -44,7 +31,7 @@ const AppLayout = () => {
                 className="me-2"
                 aria-label="Search"
               />
-              <Button variant="outline-success">Search</Button>
+              <Button variant="outline-success" className='search-btn'>Search</Button>
             </Form>
           </Navbar.Collapse>
         </Container>
