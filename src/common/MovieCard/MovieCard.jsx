653bb1c28364e42a8e4f43ec67d3514d9177443d
sangwon11 <dbstkd0408@gmail.com>
2024-09-05 04:06:34 +0900

    장르 보여주기

--- src/common/MovieCard/MovieCard.jsx
+++ src/common/MovieCard/MovieCard.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import './MovieCard.style.css';
 import { Badge } from 'react-bootstrap';
+import { useMovieGenreQuery } from '../../hooks/useMovieGenre';
 
 const MovieCard = ({movie}) => {
+
+  const { data: genreData } = useMovieGenreQuery();
+
+  const showGenre = (genreIdList) => {
+    if(!genreData) return []
+    const genreNameList = genreIdList.map((id) => {
+      const genreObj = genreData.find((genre) => genre.id === id)
+      return genreObj.name;
+    })
+    return genreNameList;
+  }
+
   return (
     <div style={{backgroundImage:'url(' + `https://media.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}` + ')'}}
     className='movie-card'
     >
       <div className='overlay'>
         <h1>{movie.title}</h1>
-        {movie.genre_ids.map((id)=><Badge bg="danger">{id}</Badge>)}
+        {showGenre(movie.genre_ids).map((genre, index) => (
+          <Badge bg="danger" key={index}>
+            {genre}
+          </Badge>
+        ))}
         <div>
           <div>{movie.adult?'over18':'under18'}</div>
         </div>
