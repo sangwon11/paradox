74ac24354d6f7bca026b5cd7f94ed97484a3e32b
sangwon11 <dbstkd0408@gmail.com>
2024-09-07 03:29:26 +0900

    trailer

--- src/pages/MovieDetail/MovieDetailPage.jsx
+++ src/pages/MovieDetail/MovieDetailPage.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { useMovieDetailQuery } from '../../hooks/useMovieDetail';
+import { useMovieVideosQuery } from '../../hooks/useMovieVideo';
 import { useNavigate, useParams } from 'react-router-dom'; 
 import './MovieDetailPage.style.css';
 import { Badge } from 'react-bootstrap';
 
 const MovieDetailPage = () => {
   const { id } = useParams();
-  const { data: movie, isLoading, error } = useMovieDetailQuery({ id });
+  const { data: movie, isLoading: isLoadingMovie, error: movieError } = useMovieDetailQuery({ id });
+  const { data: videos, isLoading: isLoadingVideos, error: videosError } = useMovieVideosQuery({ id });
+
   const navigate = useNavigate();
 
-  if (isLoading) {
+  if (isLoadingMovie || isLoadingVideos) {
     return <div>Loading...</div>;
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
+  if (movieError || videosError) {
+    return <div>Error: {movieError?.message || videosError?.message}</div>;
   }
+
+  // 예고편 중 유튜브 링크만 가져오기 (TMDB에서 제공하는 주요 사이트는 YouTube가 많음)
+  const trailer = videos.results.find(video => video.site === 'YouTube' && video.type === 'Trailer');
 
   return (
     <div className='movie-detail'>
@@ -36,8 +42,25 @@ const MovieDetailPage = () => {
         <div>
           <Badge bg="danger" >Overview</Badge> {movie.overview}
         </div>
-        <button onClick={() => navigate(`/movies/${id}/reviews`)} className='review-btn'>Reviews</button>
-        <button className='review-btn'>Trailer</button>
+        {trailer ? (
+        <div>
+          <h2>Trailer</h2>
+          <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${trailer.key}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      ) : (
+        <p>예고편을 찾을 수 없습니다.</p>
+      )}
+      <div className='review-btn-area'>
+      <button onClick={() => navigate(`/movies/${id}/reviews`)} className='review-btn'>Reviews</button>
+      </div>
       </div>
       <img className='movie-img' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
     </div>
