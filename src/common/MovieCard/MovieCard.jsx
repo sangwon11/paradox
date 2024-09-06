55167b8c6b2b697831699ccfc66e9caca875810b
sangwon11 <dbstkd0408@gmail.com>
2024-09-07 00:09:45 +0900

    gotoMovieDetailPage

--- src/common/MovieCard/MovieCard.jsx
+++ src/common/MovieCard/MovieCard.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './MovieCard.style.css';
 import { Badge } from 'react-bootstrap';
 import { useMovieGenreQuery } from '../../hooks/useMovieGenre';
+import { useNavigate } from 'react-router-dom';
 
 const MovieCard = ({movie}) => {
 
   const { data: genreData } = useMovieGenreQuery();
+  const navigate = useNavigate();
 
   const showGenre = (genreIdList) => {
     if(!genreData) return []
@@ -15,10 +17,13 @@ const MovieCard = ({movie}) => {
     })
     return genreNameList;
   }
+  const gotoMovieDetailPage = (id) => {
+    navigate(`/movies/${id}`);
+  }
 
   return (
     <div style={{backgroundImage:'url(' + `https://media.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}` + ')'}}
-    className='movie-card'
+    className='movie-card' onClick={() => gotoMovieDetailPage(movie.id)}
     >
       <div className='overlay'>
         <h1>{movie.title}</h1>
