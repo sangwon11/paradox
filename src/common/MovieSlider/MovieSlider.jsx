79c1048adb516d1568834117bb01bc3e24647857
sangwon11 <dbstkd0408@gmail.com>
2024-10-05 23:21:28 +0900

    1

--- src/common/MovieSlider/MovieSlider.jsx
+++ src/common/MovieSlider/MovieSlider.jsx
@@ -4,7 +4,6 @@ import MovieCard from '../MovieCard/MovieCard';
 import 'react-multi-carousel/lib/styles.css';
 import Carousel from 'react-multi-carousel';
 
-
 const MovieSlider = ({title, movies, responsive}) => {
   return (
     <div>
